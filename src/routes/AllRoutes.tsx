8a3b647951818e181cefed134a213a3824065930
Lucas <lucasschroeder2410@gmail.com>
2024-03-29 17:07:16 -0300

    Routes comeback :D

---
+++ src/routes/AllRoutes.tsx
@@ -0,0 +1,26 @@
+import Home from '../pages/Home';
+import Projects from '../pages/Projects';
+import { Header } from '../components/Header/Header';
+import { Footer } from '../components/Footer/Footer'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import React from 'react';
+import { Mouse } from '../components/Mouse/Mouse';
+
+export default function AllRoutes() {
+    const refMouse = React.useRef(null);
+
+    return (
+        <BrowserRouter>
+            <div className='h-screen' ref={refMouse}>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/projects" element={<Projects />} />
+                    <Route path="/about" element={<Projects />} />
+                </Routes>
+                <Footer/>
+                <Mouse refMouse={refMouse}/>
+            </div>
+        </BrowserRouter>
+    )
+}
