7454eb37d99706211b35d8cd48ddd262eb16a6a3
Lucas <lucasschroeder2410@gmail.com>
2024-03-29 14:53:54 -0300

    Margin and padding changes in home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -10,7 +10,7 @@ export default function Home() {
                 <SocialTitle/>
             </section>
 
-            <section className="max-w-5xl flex flex-col lg:flex-row gap-10 lg:gap-20 mx-auto mt-14">
+            <section className="max-w-5xl flex flex-col lg:flex-row gap-16 lg:gap-20 mx-auto mt-6 lg:mt-14">
                 <div className="w-full lg:w-1/2">
                     <AnimatedText once text={["Meu nome é Lucas Schroeder.", "Sou um desenvolvedor web com dois anos de experiência e estou cursando o último semestre da faculdade de Análise e Desenvolvimento de sistemas.",
                         "Tenho sólidos conhecimentos em desenvolvimentos de aplicações web usando JavaScript, TypeScript, NodeJS e React.",
