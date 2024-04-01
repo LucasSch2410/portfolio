10862bed18ac58c192ccd0efaefbadeba3e5fd0f
LUCAS SCHROEDER <LUCAS.SCH@pch.local>
2024-04-01 13:44:21 -0300

    Analytics Vercel section

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,12 @@ import { ToastContainer } from "react-toastify"
 import AllRoutes from "./routes/AllRoutes"
 import 'react-toastify/dist/ReactToastify.css';
 import { MouseProvider } from "./context/mouseContext";
+import { Analytics } from "@vercel/analytics/react";
 
 export default function App() {
     return (
         <>
+            <Analytics />
             <ToastContainer />
             <MouseProvider>
                 <AllRoutes />
